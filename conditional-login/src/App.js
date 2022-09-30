5557086270b04e52d6b91f188755979f068decea
faheem4531 <aa0561085@gmail.com>
2022-09-30 15:53:10 +0500

    Ternary operator

--- conditional-login/src/App.js
+++ conditional-login/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import Form from './components/form/Form';
 
-var isLoggedIn = false;
-
-
-function checkCondition() {
-  if (isLoggedIn === true) {
-    return <h1>Hello User</h1>
-  }
-  else if (isLoggedIn === false) {
-    return <Form />
-  }
-}
-
+var isLoggedIn = true;
 
 function App() {
   return (
     <div className="container">
-      {checkCondition()}
+      {
+        //Ternary Operator
+        // condition ? do if true : do if false
+
+        isLoggedIn ? <h1>Hello User</h1> : <Form />
+      }
     </div>
   )
 }
