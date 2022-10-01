8128ccd1454dc5122dd90288cab42a54faa5c03f
faheem4531 <aa0561085@gmail.com>
2022-10-02 02:03:27 +0500

    Click button to update time function

--- conditional-login/src/components/time/Time.js
+++ conditional-login/src/components/time/Time.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
 
 const Time = () => {
 
 
-  const now = new Date().toLocaleDateString();
+  const now = new Date().toLocaleTimeString();
 
+  const [time , setTime ] = useState(now);
+
+  function updateTime() {
+    const newTime =  new Date().toLocaleTimeString();
+    setTime(newTime)
+  }
 
   return (
     <div>
     <h1 className="welcome-heading">Welcome</h1>
 
     <div className="time-div">
-      <h1>Time</h1>
-      <button className="btn-time">Time</button>
+      <h1>{now}</h1>
+      <button onClick={updateTime} className="btn-time">Update Time</button>
     </div>
     
     </div>
