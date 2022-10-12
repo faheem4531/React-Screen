7e31231ed23a8990d301479da683c218106a01fe
faheem4531 <aa0561085@gmail.com>
2022-10-12 23:32:46 +0500

    Delete useless files

--- Single home screen/src/App.js
+++ Single home screen/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { HomeContainer } from './container';
 
 function App() {
   return (
     <div>
-      <HomeContainer/>
+      <HomeContainer />
     </div>
   );
 }
